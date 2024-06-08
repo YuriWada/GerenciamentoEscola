eedbb4ca5108d0a6b58c2b06fe97782f99050702
Yuri Wada <yuri.wadah@gmail.com>
2024-06-08 10:55:45 -0300

    Initial system configuration and main file creation

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+class SchoolSystem:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        try:
+            print('Hello World')
+        except:
+            pass
+
+if __name__ == '__main__':
+    schoolsystem = SchoolSystem()
+    schoolsystem.run()
