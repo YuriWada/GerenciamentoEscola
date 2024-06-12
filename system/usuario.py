e7f6f2b94ea0254e7e4b4f5c8c505828fbf7733c
raposoxxx <raposojulia04@gmail.com>
2024-06-12 18:48:56 -0300

    feat: new class format

--- system/usuario.py
+++ system/usuario.py
@@ -7,13 +7,14 @@ Author: Julia Raposo
 
 
 class Usuario:
-    def __init__(self, nome : str, idade : int, endereco : str, telefone: str, email : str, login : str) -> None:
+    def __init__(self, nome : str, idade : int, endereco : str, telefone: str, email : str, login : str, nivel : int) -> None:
         self.nome = nome
         self.idade = idade
         self.endereco = endereco
         self.telefone = telefone
         self.email = email
         self._login = login
+        self.nivel = nivel
 
     '''def excluir_conta(self) -> None:'''
 
@@ -21,31 +22,40 @@ class Usuario:
 
 
 class Aluno(Usuario):
-    def __init__(self, nome : str, idade : int, endereco : str, telefone: str, email : str, login : str, curso : str, matricula : str) -> None:
-        super().__init__(nome, idade, endereco, telefone, email, login)
+    def __init__(self, nome : str, idade : int, endereco : str, telefone: str, email : str, login : str, curso : str, matricula : str, nivel : int) -> None:
+        super().__init__(nome, idade, endereco, telefone, email, login, nivel)
         self.curso = curso
         self.matricula = matricula
+
 
     '''def ver_notas() -> None:'''
 
     '''def ver_horario() -> None:'''
 
 
-
-class Professor(Usuario):
-    def __init__(self, nome : str, idade : int, endereco : str, telefone: str, email : str, login : str, materia : str, salario : int) -> None:
-        super().__init__(nome, idade, endereco, telefone, email, login)
-        self.materia = materia
-        self.salario = salario
-
-    '''def adicionar_nota() -> None:'''
-
-    '''def alterar_nota() -> None:'''
-
-
 class Staff(Usuario):
-    def __init__(self,nome : str, idade : int, endereco : str, telefone: str, email : str, login : str, funcao : str, salario : int) -> None:
-        super().__init__(nome, idade, endereco, telefone, email, login)
+    def __init__(self, nome: str, idade: int, endereco: str, telefone: str, email: str, login: str, funcao : str, salario: str, nivel : int) -> None:
+        super().__init__(nome, idade, endereco, telefone, email, login, nivel)
         self.funcao = funcao
         self.salario = salario
+
+
+class Professor(Staff):
+    def __init__(self, nome: str, idade: int, endereco: str, telefone: str, email: str, login: str, funcao: str, salario: str, materia: str, nivel : int) -> None:
+        super().__init__(nome, idade, endereco, telefone, email, login, funcao, salario, nivel)
+        self.materia = materia
+    
+    '''def adicionar_notas()'''
+
+    '''def alterar_notas()'''
+
+class CorpoEstudantil(Staff):
+    def __init__(self, nome: str, idade: int, endereco: str, telefone: str, email: str, login: str, funcao: str, salario: str) -> None:
+        super().__init__(nome, idade, endereco, telefone, email, login, funcao, salario)
+
+    
+
+
+
+
 
