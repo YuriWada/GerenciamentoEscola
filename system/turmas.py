cce330f9bbcc4f427ecad1a66107785288553b13
raposoxxx <juliarferreira00@gmail.com>
2024-06-13 11:46:19 -0300

    fix: turmas

--- system/turmas.py
+++ system/turmas.py
@@ -2,7 +2,6 @@ import pymongo
 from usuario import Professor, Aluno
 from abc import ABC
 
-'''class Turmas(ABC):
-    def __init__(self):
-        self.professor = Professor()
-        self.disciplina = '''
+class Turmas(ABC):
+    def criar_turma() -> None:
+        
