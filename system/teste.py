78acf0f796315e6fb2b7eb2900624019503f6550
Yuri Wada <yuri.wadah@gmail.com>
2024-06-19 00:25:53 -0300

    Dummy data

--- system/teste.py
+++ system/teste.py
@@ -7,30 +7,6 @@ from cadastrodiretoria import CadastroDiretoria
 from cadastroalunodisciplina import CadastroAlunoDisciplina
 from criadisciplina import CriaDisciplina
 
-class TesteCadastro:
-    def testar_cadastro(self, tipo : str, nome : str, idade : int, endereco : str, telefone : str, email : str, login : str,
-                        senha : str, curso : str = None, matricula : str = None, disciplina : str = None, cargo : str = None) -> None:
-        try:
-            if tipo == "aluno":
-                if curso is None or matricula is None:
-                    raise ValueError("Insira curso e matrícula")
-                cadastro = CadastroAluno(nome, idade, endereco, telefone, email, login, senha, curso, matricula, self.db)
-            elif tipo == "professor":
-                if disciplina is None:
-                    raise ValueError("Insira disciplina")
-                cadastro = CadastroProfessor(nome, idade, endereco, telefone, email, login, senha, disciplina, self.db)
-            elif tipo == "diretoria":
-                if cargo is None:
-                    raise ValueError("Insira o cargo")
-                cadastro = CadastroDiretoria(nome, idade, endereco, telefone, email, login, senha, cargo, self.db)
-            else:
-                raise ValueError("Tipo de cadastro inválido. Deve ser 'aluno', 'professor' ou 'diretoria'!!")
-            
-            cadastro.save()
-            print(f"Cadastro de {tipo} testado com sucesso.")
-        except Exception as e:
-            print(f"Erro ao testar cadastro de {tipo}: {e}")
-
 # Testes
 
 db = DataBase()
